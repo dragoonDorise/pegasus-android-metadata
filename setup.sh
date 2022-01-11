a4bd83eab9f62a7fcdc41d242e03ae369f575977
Rodrigo <rodrigosedano@me.com>
2022-01-11 21:05:31 +0100

    glsl

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@
 echo "Hi! We're gonna start configuring your Android Device, if the systems asks you any question, just press y and enter :)"
 echo "Press Enter to start"
 read pausa
-pkg update -y && pkg upgrade -y
-pkg install x11-repo build-essential qt5-qtbase -y 
-pkg git -y
+#pkg update -y && pkg upgrade -y
+#pkg install x11-repo build-essential qt5-qtbase -y 
+pkg install git wget rsync -y
 termux-setup-storage
 mkdir ~/dragoonDoriseTools
 cd dragoonDoriseTools
@@ -13,27 +13,29 @@ cd dragoonDoriseTools
 
 #Download Pegasus Metadata files
 git clone https://github.com/dragoonDorise/pegasus-android-metadata.git pegasus-android-metadata/ 
-git clone https://github.com/muldjord/skyscraper.git skyscraper
+#git clone https://github.com/muldjord/skyscraper.git skyscraper
 
 #Download Pegasus
-curl https://github.com/mmatyas/pegasus-frontend/releases/download/weekly_2021w40/pegasus-fe_alpha15-85-gfff1a5b2_android.apk --output pegasus.15.85.apk
+wget https://github.com/mmatyas/pegasus-frontend/releases/download/weekly_2021w40/pegasus-fe_alpha15-85-gfff1a5b2_android.apk
 
 #Install Skyscraper
-
-
-cd skyscraper
-qmake
-make
-chmod a+rwx Skyscraper
-cp Skyscraper /data/data/com.termux/files/usr/bin
-cp ~/dragoonDoriseTools/pegasus-android-metadata/internal/common/artwork.xml /data/data/com.termux/files/usr/bin
-cd ..
+#cd skyscraper
+#qmake
+#make
+#chmod a+rwx Skyscraper
+#cp Skyscraper /data/data/com.termux/files/usr/bin
+#cp ~/dragoonDoriseTools/pegasus-android-metadata/internal/common/artwork.xml /data/data/com.termux/files/usr/bin
+#cd ..
 
 #Configure Pegasus
 mkdir ~/storage/shared/pegasus-frontend
 mkdir ~/storage/shared/pegasus-frontend/themes
 cp ~/dragoonDoriseTools/pegasus-android-metadata/internal/common/pegasus-frontend/settings.txt ~/storage/shared/pegasus-frontend
 cp ~/dragoonDoriseTools/pegasus-android-metadata/internal/common/pegasus-frontend/game_dirs.txt ~/storage/shared/pegasus-frontend
+
+#Configure Retroarch
+rsync -r ~/dragoonDoriseTools/pegasus-android-metadata/internal/common/RetroArch/config/ ~/storage/shared/RetroArch/config/
+
 
 # Install Themes on Pegasus
 git clone https://github.com/dragoonDorise/RP-epic-noir.git ~/storage/shared/pegasus-frontend/themesRP-epic-noir
@@ -42,4 +44,4 @@ git clone https://github.com/dragoonDorise/RP-switch.git ~/storage/shared/pegasu
 #Configure RetroArch
 
 #Launch Pegasus
-xdg-open pegasus.15.85.apk
+xdg-open pegasus-fe_alpha15-85-gfff1a5b2_android.apk
