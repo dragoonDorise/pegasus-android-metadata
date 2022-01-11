565d60b0b98102655ff77436b705528cf3aaf6f6
Rodrigo <rodrigosedano@me.com>
2022-01-12 00:13:43 +0100

    Scrapper for GB

--- scrap.sh
+++ scrap.sh
@@ -1,26 +1,10 @@
 #!/bin/sh
-
-git clone https://github.com/dragoonDorise/pegasus-artwork.git ./pegasus-artwork
+#
+#git clone https://github.com/dragoonDorise/pegasus-artwork.git ./pegasus-artwork
 
 selected_device_descriptions=$(whiptail --title "Pegasus Rom Scrapper" \
    --checklist "Please Select your platform with the Space" 10 80 4 \
-		atari2600 "" ON \
-		"dreamcast" "dreamcast" OFF \
-		"gamegear" "gamegear" OFF \
-		"gb" "gb" OFF \
-		"gba" "gba" OFF \
-		"gbc" "gbc" OFF \
-		"gamecube" "gc" OFF \
-		"genesis" "genesis" OFF \
-		"mastersystem" "mastersystem" OFF \
-		"n64" "n64" OFF \
-		"nes" "nes" OFF \
-		"pcengine" "pcengine" OFF \
-		"psx" "psx" OFF \
-		"saturn" "saturn" OFF \
-		"sega32x" "sega32x" OFF \
-		"segacd" "segacd" OFF \
-		"snes" "snes" OFF \
+		"gb" "" ON \
    3>&1 1<&2 2>&3)
 
 mapfile -t selected_device_names <<< $selected_device_descriptions
@@ -28,8 +12,65 @@ mapfile -t selected_device_names <<< $selected_device_descriptions
 for device_name in ${selected_device_names[@]};
  do
 	 message=$device_name
-	 system="${message//'"'/}"            
-   rsync -r ~/pegasus-artwork/$system/ ~/storage/external-1/$system/
+	  system="${message//'"'/}"            
+	 #ls ~/storage/external-1/$system
+	 mkdir ~/storage/external-1/$system/media
+	 mkdir ~/storage/external-1/$system/media/screenshot
+	 mkdir ~/storage/external-1/$system/media/box2dfront
+	 mkdir ~/storage/external-1/$system/media/wheel
+	 for entry in ~/storage/external-1/$system/*
+	 do
+	 
+	 #	echo $entry;
+		 firstString=$entry
+		 secondString=".png"
+		 capture="${firstString/.7z/"$secondString"}"   
+		 
+		firstString=$capture
+		 secondString=""
+		 capture="${firstString/"/data/data/com.termux/files/home/storage/external-1/$system/"/"$secondString"}"   
+		 
+				
+		FILE=~/storage/external-1/$system/media/screenshot/$capture
+		if [ -f "$FILE" ]; then
+			echo "$FILE exists, ignoring"
+		else 
+			wget  "http://thumbnails.libretro.com/Nintendo - Game Boy/Named_Snaps/$capture" -P ~/storage/external-1/$system/media/screenshot/
+		fi
+		
+		FILE=~/storage/external-1/$system/media/box2dfront/$capture
+		if [ -f "$FILE" ]; then
+			echo "$FILE exists, ignoring"
+		else 
+			wget  "http://thumbnails.libretro.com/Nintendo - Game Boy/Named_Boxarts/$capture" -P ~/storage/external-1/$system/media/box2dfront/
+		fi
+		
+		FILE=~/storage/external-1/$system/media/wheel/$capture
+		if [ -f "$FILE" ]; then
+			echo "$FILE exists, ignoring"
+		else 
+			wget  "http://thumbnails.libretro.com/Nintendo - Game Boy/Named_Titles/$capture" -P ~/storage/external-1/$system/media/wheel/
+		fi
+		
+				 
+				
+				
+		
+		
+		
+		#for rom in $entry
+		#do
+		#	firstString=$rom
+		 #	secondString=".png"
+		 #	echo capture="${firstString/.7z/"$secondString"}"             
+		#
+		#done
+	 #	
+	 
+	  # wget http://thumbnails.libretro.com/$system/Named_Snaps/$capture
+	 done
+	 
+   #rsync -r ~/pegasus-artwork/$system/ ~/storage/external-1/$system/
  done
  
- rm -rf pegasus-artwork
+ #rm -rf pegasus-artwork
