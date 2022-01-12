aee5b17a70d9f9bcbb8f286f4174c38e861bbaf2
Rodrigo <rodrigosedano@me.com>
2022-01-12 22:41:09 +0100

    Fix selector

--- scrap.sh
+++ scrap.sh
@@ -3,40 +3,40 @@
 
 selected_device_descriptions=$(whiptail --title "Pegasus Rom Scrapper" \
    --checklist "Move using yout DPAD and select your platforms with the Space" 10 80 4 \
-	"atari2600" "Atari - 2600" ON \
-	"atarilynx" "Atari - Lynx" ON \
-	"doom" "DOOM" ON \
-	"dos" "DOS" ON \
-	"fbneo" "FBNeo - Arcade Games" ON \
-	"pcengine" "NEC - PC Engine - TurboGrafx 16" ON \
-	"pcenginecd" "NEC - PC Engine CD - TurboGrafx-CD" ON \
-	"gb" "Nintendo - Game Boy" ON \
-	"gba" "Nintendo - Game Boy Advance" ON \
-	"gbc" "Nintendo - Game Boy Color" ON \
-	"gc" "Nintendo - GameCube" ON \
-	"3ds" "Nintendo - Nintendo 3DS" ON \
-	"n64" "Nintendo - Nintendo 64" ON \
-	"nds" "Nintendo - Nintendo DS" ON \
-	"nes" "Nintendo - Nintendo Entertainment System" ON \
-	"pokemini" "Nintendo - Pokemon Mini" ON \
-	"snes" "Nintendo - Super Nintendo Entertainment System" ON \
-	"wii" "Nintendo - Wii" ON \
-	"neogeo" "SNK - Neo Geo" ON \
-	"neogeocd" "SNK - Neo Geo CD" ON \
-	"ngp" "SNK - Neo Geo Pocket" ON \
-	"ngpc" "SNK - Neo Geo Pocket Color" ON \
-	"scummvm" "ScummVM" ON \
-	"sega32x" "Sega - 32X" ON \
-	"dreamcast" "Sega - Dreamcast" ON \
-	"gamegear" "Sega - Game Gear" ON \
-	"mastersystem" "Sega - Master System" ON \
-	"genesis" "Sega - Mega Drive - Genesis" ON \
-	"segacd" "Sega - Mega-CD - Sega CD" ON \
-	"saturn" "Sega - Saturn" ON \
-	"psx" "Sony - PlayStation" ON \
-	"ps2" "Sony - PlayStation 2" ON \
-	"psp" "Sony - PlayStation Portable" ON \
-	"3do" "The 3DO Company - 3DO" ON \
+	"atari2600" "Atari - 2600" OFF \
+	"atarilynx" "Atari - Lynx" OFF \
+	"doom" "DOOM" OFF \
+	"dos" "DOS" OFF \
+	"fbneo" "FBNeo - Arcade Games" OFF \
+	"pcengine" "NEC - PC Engine - TurboGrafx 16" OFF \
+	"pcenginecd" "NEC - PC Engine CD - TurboGrafx-CD" OFF \
+	"gb" "Nintendo - Game Boy" OFF \
+	"gba" "Nintendo - Game Boy Advance" OFF \
+	"gbc" "Nintendo - Game Boy Color" OFF \
+	"gc" "Nintendo - GameCube" OFF \
+	"3ds" "Nintendo - Nintendo 3DS" OFF \
+	"n64" "Nintendo - Nintendo 64" OFF \
+	"nds" "Nintendo - Nintendo DS" OFF \
+	"nes" "Nintendo - Nintendo Entertainment System" OFF \
+	"pokemini" "Nintendo - Pokemon Mini" OFF \
+	"snes" "Nintendo - Super Nintendo Entertainment System" OFF \
+	"wii" "Nintendo - Wii" OFF \
+	"neogeo" "SNK - Neo Geo" OFF \
+	"neogeocd" "SNK - Neo Geo CD" OFF \
+	"ngp" "SNK - Neo Geo Pocket" OFF \
+	"ngpc" "SNK - Neo Geo Pocket Color" OFF \
+	"scummvm" "ScummVM" OFF \
+	"sega32x" "Sega - 32X" OFF \
+	"dreamcast" "Sega - Dreamcast" OFF \
+	"gamegear" "Sega - Game Gear" OFF \
+	"mastersystem" "Sega - Master System" OFF \
+	"genesis" "Sega - Mega Drive - Genesis" OFF \
+	"segacd" "Sega - Mega-CD - Sega CD" OFF \
+	"saturn" "Sega - Saturn" OFF \
+	"psx" "Sony - PlayStation" OFF \
+	"ps2" "Sony - PlayStation 2" OFF \
+	"psp" "Sony - PlayStation Portable" OFF \
+	"3do" "The 3DO Company - 3DO" OFF \
    3>&1 1<&2 2>&3)
 
 mapfile -t selected_device_names <<< $selected_device_descriptions
