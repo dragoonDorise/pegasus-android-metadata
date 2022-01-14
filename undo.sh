e03b9da0ac430da59de4b0378e7ee4d1a2451443
Rodrigo <rodrigosedano@me.com>
2022-01-14 20:51:37 +0100

    fix deleting retroarch

--- undo.sh
+++ undo.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+NONE='\033[00m'
+RED='\033[01;31m'
+GREEN='\033[01;32m'
+YELLOW='\033[01;33m'
+PURPLE='\033[01;35m'
+CYAN='\033[01;36m'
+WHITE='\033[01;37m'
+BOLD='\033[1m'
+UNDERLINE='\033[4m'
 
 rm -rf ~/dragoonDoriseTools
 
@@ -14,21 +23,16 @@ rm -f undo.sh
 rm -f ~/dragoonDoriseTools/pegasus-fe_alpha15-85-gfff1a5b2_android.apk
 
 #Restoring Retroarch
-rm -f ~/storage/shared/RetroArch/config/
-
 FILE=~/storage/shared/RetroArch/config_bak
 if [ -f "$FILE" ]; then
 	rm -rf ~/storage/shared/RetroArch/config/
 	cp ~/storage/shared/RetroArch/config_bak/ ~/storage/shared/RetroArch/config/
 	rm ~/storage/shared/RetroArch/config_bak/
-else 
-	
-	
 fi
 
 clear
 
-echo "All cleaned up!"
+echo "${GREEN}All cleaned up!${NONE}"
 echo "We Hope to see you come back soon!"
 echo "NOTE: Termux and Pegasus have to be uninstalled like any other Android app. Pegasus themes are not deleted either"
 
