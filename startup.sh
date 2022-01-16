f6281c233eb80a9a9926565f960db61b3f808b79
Rodrigo <rodrigosedano@me.com>
2022-01-16 20:34:13 +0100

    radiolist

--- startup.sh
+++ startup.sh
@@ -12,13 +12,12 @@ BOLD='\033[1m'
 UNDERLINE='\033[4m'
 BLINK='\x1b[5m'
 
-selected_option=$(whiptail --title "Pegasus Installer Menu" \
-   --checklist "Move using your DPAD and select your option with the Space. Press the A button when ready." 10 80 4 \
-	"1" "Reinstall Pegasus Metadata" OFF \
-	"2" "Update Pegasus Metadata & Themes" OFF \
-	"3" "Scrap your Roms" OFF \	
-	"4" "Open CLI" OFF \	
-	"5" "Exit" OFF \	
-   3>&1 1<&2 2>&3)
+selected_option=$(whiptail --title "Pegasus Installer Menu" --radiolist "Move using your DPAD and select your option with the Space. Press the A button when ready." 20 40 15 \
+"1" "Reinstall Pegasus Metadata" OF \
+"2" "Update Pegasus Metadata & Themes" OFF \
+"3" "Scrap your Roms" OFF \
+"4" "Open Termux CLI" OFF \
+"5" "Exit" OFF \
+3>&1 1>&2 2>&3)
 
 echo $selected_option
