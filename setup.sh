e213ae14d4dbeae122f2dfea625b8f6ad35a333a
Rodrigo <rodrigosedano@me.com>
2022-01-18 17:31:33 +0100

    Emulator detection + installation

--- setup.sh
+++ setup.sh
@@ -27,6 +27,7 @@ hasMupen=false
 hasPPSSPP=false
 hasDuckstation=false
 hasDrastic=false
+hasDolphin=false
 #Retroarch?
 FOLDER=~/storage/shared/Android/data/com.retroarch
 if [ -d "$FOLDER" ]; then
@@ -61,6 +62,11 @@ fi
 FOLDER=~/storage/shared/DraStic
 if [ -d "$FOLDER" ]; then
 	hasDrastic=true
+fi
+#Dolphin
+FOLDER=~/storage/shared/dolphin-mmj
+if [ -d "$FOLDER" ]; then
+	hasDolphin=true
 fi
 clear
 echo -ne "Installing components, please be patient..."
@@ -236,39 +242,69 @@ if [ hasRedDream==true ]; then
 	echo -e  "${GREEN}Installed${NONE}"
 else
 	echo -e  "${RED}Not installed${NONE}"
+	echo "Type Y and press the A Button if you want to install it. A link to the Play Store will open"
+	read pause
+	termux-open "https://play.google.com/store/apps/details?id=io.recompiled.redream"
 fi
 echo -ne "Saturn - Yaba Sanshioro 2..."
 if [ hasYaba==true ]; then
 	echo -e  "${GREEN}Installed${NONE}"
-else
+	else
 	echo -e  "${RED}Not installed${NONE}"
+	echo "Type Y and press the A Button if you want to install it. A link to the Play Store will open"
+	read pause
+	termux-open "https://play.google.com/store/apps/details?id=org.devmiyax.yabasanshioro2"
 fi
 echo -ne "Nintendo 64 - Mupen 64 Plus..."
 if [ hasMupen==true ]; then
 	echo -e  "${GREEN}Installed${NONE}"
-else
+	else
 	echo -e  "${RED}Not installed${NONE}"
+	echo "Type Y and press the A Button if you want to install it. A link to the Play Store will open"
+	read pause
+	termux-open "https://play.google.com/store/apps/details?id=org.mupen64plusae.v3.fzurita"
 fi
 echo -ne "PSP - PPSSPP..."
 if [ hasPPSSPP==true ]; then
 	echo -e  "${GREEN}Installed${NONE}"
-else
+	else
 	echo -e  "${RED}Not installed${NONE}"
+	echo "Type Y and press the A Button if you want to install it. A link to the Play Store will open"
+	read pause
+	termux-open "https://play.google.com/store/apps/details?id=org.ppsspp.ppsspp"
 fi
 echo -ne "Playstation - Duckstation..."
 if [ hasDuckstation==true ]; then
 	echo -e  "${GREEN}Installed${NONE}"
-else
+	else
 	echo -e  "${RED}Not installed${NONE}"
+	echo "Type Y and press the A Button if you want to install it. A link to the Play Store will open"
+	read pause
+	termux-open "https://play.google.com/store/apps/details?id=com.github.stenzek.duckstation"
 fi
 echo -ne "Nintendo DS - Drastic..."
 if [ hasDrastic==true ]; then
 	echo -e  "${GREEN}Installed${NONE}"
 else
 	echo -e  "${RED}Not installed${NONE}"
+	echo "Type Y and press the A Button if you want to install it. A link to the Play Store will open"
+	read pause
+	termux-open "https://play.google.com/store/apps/details?id=com.dsemu.drastic"
 fi
 
-echo -e "${BOLD}Remember to install those emulators if you want to play those systems${NONE}"
+echo -ne "Nintendo Wii & GameCube - Dolphin MMJR..."
+if [ hasDrastic==true ]; then
+	echo -e  "${GREEN}Installed${NONE}"
+else
+	echo -e  "${RED}Not installed${NONE}"
+	echo "Type Y and press the A Button if you want to install it."
+	read pause
+	wget  -q --show-progress https://github.com/Bankaimaster999/Dolphin-MMJR/releases/download/1.0-11460/Dolphin.MMJR.v11460.apk ~/dragoonDoriseTools/
+	xdg-open ~/dragoonDoriseTools/Dolphin.MMJR.v11460.apk
+fi
+
+
+echo -e "${BOLD}If you chose to not install the emulator, remember that you need them if you want to play those systems${NONE}"
 
 echo -e  "";
 echo -e  "${GREEN}All Done${NONE}, do you have your SD Card inserted with all your roms?"
