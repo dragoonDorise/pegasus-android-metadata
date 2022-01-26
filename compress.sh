9ea1fd60408d8c93919ca56a50aea861e64f70e8
SoumyBhow <soumybhow1.0@gmail.com>
2022-01-25 22:20:21 -0500

    Update Compress.sh

--- compress.sh
+++ compress.sh
@@ -11,10 +11,10 @@ BOLD='\033[1m'
 UNDERLINE='\033[4m'
 BLINK='\x1b[5m'
 clear
+
 useInternalStorage=false
 enableCompression=false
 FILE=~/dragoonDoriseTools/.storageInternal
-
 if [ -f "$FILE" ]; then
 	useInternalStorage=true
 	storageLocation="shared/roms"
@@ -27,12 +27,42 @@ fi
 
 
 if (whiptail --title "Rom compressor" --yesno "Would you like to compress your roms now?" 10 80); then
+selected_device_descriptions=$(whiptail --title "Pegasus Rom Compressor" \
+   --checklist "Move using your DPAD and select your options with the Y button. Press the A button to select." 10 80 5 \
+	"dreamcast" "Sega - Dreamcast" OFF \
+	"saturn" "Sega - Saturn" OFF \
+	"psx" "Sony - PlayStation" OFF \
+	"ps2" "Sony - PlayStation 2" OFF \
+
+   3>&1 1<&2 2>&3)
+for device_name in ${selected_device_names[@]};
+ 		do
+	 		message=$device_name
+	 		system="${message//'"'/}"          
+cd ~/storage/$storageLocation/$system
 
 
+#Convert cue to chd
+for f in ./*.cue
+do
+	name=${f%.cue} # Remove '.cue' from file name
+	chdman createcd -i "$name.cue" -o "$name.chd" --force
+	#rm -rf "$name.cue"
+	#rm -rf "$name.bin"
+done
+
+#Convert iso to chd
+for f in ./*.iso
+do
+	name=${f%.iso} # Remove '.iso' from file name
+	chdman createcd -i "$name.iso" -o "$name.chd" --force
+	#rm -rf "$name.iso"
+	
+done
+done
 else
 echo -e "User chose not to compress roms. If you change your mind you can always run this script again \n"
 read -n 1 -r -s -p $'Press any key to continue...\n' 
 fi
 
-
-done
+exit
