07b8126160843e24d3f90a0ddee9a98e737c001f
Rodrigo <rodrigosedano@me.com>
2022-03-06 19:03:33 +0100

    More logs

--- update.sh
+++ update.sh
@@ -13,8 +13,11 @@ BLINK='\x1b[5m'
 clear
 echo -ne  "Updating Updater..."
 cd ~/dragoonDoriseTools/pegasus-android-metadata/ &>> ~/storage/shared/pegasus_installer_log.log 
+echo "### Update: cd metadata directory "  &>> ~/storage/shared/pegasus_installer_log.log
 git reset --hard &>> ~/storage/shared/pegasus_installer_log.log 
+echo "### Update: git reset "  &>> ~/storage/shared/pegasus_installer_log.log
 git pull &>> ~/storage/shared/pegasus_installer_log.log 
+echo "### Update: git pull "  &>> ~/storage/shared/pegasus_installer_log.log
 cp ~/dragoonDoriseTools/pegasus-android-metadata/update.sh ~/update.sh
 chmod a+rwx ~/update.sh
 cp ~/dragoonDoriseTools/pegasus-android-metadata/run_update.sh ~/run_update.sh
@@ -27,6 +30,7 @@ cp ~/dragoonDoriseTools/pegasus-android-metadata/startup.sh  ~/startup.sh &>> ~/
 chmod a+rwx ~/startup.sh &>> ~/storage/shared/pegasus_installer_log.log
 cp ~/dragoonDoriseTools/pegasus-android-metadata/snes_config.sh  ~/snes_config.sh &>> ~/storage/shared/pegasus_installer_log.log
 chmod a+rwx ~/snes_config.sh &>> ~/storage/shared/pegasus_installer_log.log
+echo "### Update: cp .sh "  &>> ~/storage/shared/pegasus_installer_log.log
 
 echo -e "${GREEN}OK${NONE}"
 /bin/bash ~/run_update.sh
