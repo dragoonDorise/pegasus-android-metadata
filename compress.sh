568dfd77c9834a82569f5fe3b35c92fce9eee131
SoumyBhow <soumybhow1.0@gmail.com>
2022-05-20 22:12:08 -0400

    Initial compress.sh (incomplete)

---
+++ compress.sh
@@ -0,0 +1,55 @@
+#Functions for CHDMAN & Maxcso
+compressCHDMAN () {
+    for file in ./*.{iso,cue}; do
+    name="${file%.*}" #Remove extension
+    chdman createcd -i "file" -o "name.chd" --force
+done
+
+compressMaxcso () {
+    for file in ./*.{iso,cue}; do
+    maxcso "$file"
+done
+
+#Check for .storageInternal and determine if ROMs are in internal or not
+useInternalStorage=false
+FILE=~/dragoonDoriseTools/.storageInternal
+if [ -f "$FILE" ]; then
+	useInternalStorage=true
+	storageLocation="shared/roms"
+else
+	useInternalStorage=false
+	storageLocation="external-1"
+
+fi
+
+#Pick the systems
+SYSTEMS=$(whiptail --title "ROM Compressor" --checklist "Please select the systems whose ROMs you want to compress" 20 78 15 \
+	"dreamcast" "Sega - Dreamcast" OFF \
+	"ps2" "Sony - PlayStation 2" OFF \
+	"psp" "Sony - PlayStation Portable" OFF \
+	"psx" "Sony - PlayStation" OFF \
+	"saturn" "Sega - Saturn" OFF \
+ 3>&1 1>&2 2>&3)
+
+
+#Option to delete original uncompressed ROMs
+while true; do
+	removeOldROM=$(whiptail --title "Do you want to automatically PERMANENTELY delete the uncompressed ROMs?" \
+   --radiolist "Move using your DPAD and select your platforms with the Y button. Press the A button to select." 10 80 4 \
+	"YES" "Compress my roms!" OFF \
+	"NO" "You can always do the scraping later by opening Termux" OFF \
+   3>&1 1<&2 2>&3)
+	case $compressNow in
+		[YES]* ) break;;
+		[NO]* ) break;;
+		* ) echo "Please answer yes or no.";;
+	esac
+
+for system in $(eval echo "${SYSTEMS}"); do
+    cd ~/storage/$storageLocation/$system/
+    if [ "$system" = psp ]; then
+    compressMaxcso
+    else
+    compressCHDMAN
+    fi
+done
